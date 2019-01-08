619fa5701cbb9885d45d9e5ba9b1cb90bff19ce7
kash03 <39040108+kash03@users.noreply.github.com>
2019-01-08 14:22:50 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+function firstFunc() {
+	document.getElementById('demo').innerHTML="Need to MEETUP soon";
+
+}
+$(document).ready(function(){
+	$("h1").click(function() {
+		alert(" Its an emergency do not ignore. There's alot to discuss!");
+		// body...
+	});
+});
